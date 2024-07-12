10c5c5a9cada51c07dda1d8cce453bfd70af875e
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-12 11:11:15 +0800

    feat: add data handler for reading and updating messages

---
+++ src/data/handler.ts
@@ -0,0 +1,128 @@
+export interface DataConfig {
+    version: Version;
+    vote: Vote;
+    blacklist: Vote;
+    ip: Ip;
+}
+
+interface Ip {
+    content: string;
+}
+
+interface Vote {
+    messageId: string;
+}
+
+interface Version {
+    bedrock: string;
+    java: string;
+}
+
+const defaultData = {
+    version: {
+        bedrock: "1.20.80-1.21.0",
+        java: "1.18-1.21.x",
+    },
+    vote: {
+        messageId: "",
+    },
+    blacklist: {
+        messageId: "",
+    },
+    ip: {
+        content: "play.nauticalmc.xyz",
+    },
+};
+
+export const readMessages = async () => {
+    const file = Bun.file(Bun.pathToFileURL("data/messages.json"));
+
+    if (!(await file.exists())) {
+        throw new Error("The data file does not exist.");
+    }
+
+    const data: DataConfig = await file.json();
+
+    return data;
+};
+
+export const updateVersion = async (bedrock: string, java: string) => {
+    const file = Bun.file(Bun.pathToFileURL("data/messages.json"));
+
+    if (!(await file.exists())) {
+        throw new Error("The packagedata file does not exist.");
+    }
+
+    const data: DataConfig = await file.json();
+
+    const config = {
+        ...data,
+        version: {
+            bedrock,
+            java,
+        },
+    };
+
+    await Bun.write(Bun.file(Bun.pathToFileURL("data/messages.json")), JSON.stringify(config, null, 4));
+};
+
+export const updateVoteMessage = async (messageId: string) => {
+    const file = Bun.file(Bun.pathToFileURL("data/messages.json"));
+
+    if (!(await file.exists())) {
+        throw new Error("The data file does not exist.");
+    }
+
+    const data: DataConfig = await file.json();
+
+    const config = {
+        ...data,
+        vote: {
+            messageId,
+        },
+    };
+
+    await Bun.write(Bun.file(Bun.pathToFileURL("data/messages.json")), JSON.stringify(config, null, 4));
+};
+
+export const updateBlacklistMessage = async (messageId: string) => {
+    const file = Bun.file(Bun.pathToFileURL("data/messages.json"));
+
+    if (!(await file.exists())) {
+        throw new Error("The data file does not exist.");
+    }
+
+    const data: DataConfig = await file.json();
+
+    const config = {
+        ...data,
+        blacklist: {
+            messageId,
+        },
+    };
+
+    await Bun.write(Bun.file(Bun.pathToFileURL("data/messages.json")), JSON.stringify(config, null, 4));
+};
+
+export const updateIp = async (content: string) => {
+    const file = Bun.file(Bun.pathToFileURL("data/messages.json"));
+
+    if (!(await file.exists())) {
+        throw new Error("The data file does not exist.");
+    }
+
+    const data: DataConfig = await file.json();
+
+    const config = {
+        ...data,
+        ip: {
+            content,
+        },
+    };
+
+    await Bun.write(Bun.file(Bun.pathToFileURL("data/messages.json")), JSON.stringify(config, null, 4));
+};
+
+export const resetData = async () => {
+    await Bun.write(Bun.file(Bun.pathToFileURL("data/messages.json")), JSON.stringify(defaultData, null, 4));
+};
